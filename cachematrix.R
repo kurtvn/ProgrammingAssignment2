d8373c4991eb92e41fe6c20a7bd7527cb9d53da7
Kurt von Nieda <kurtvn@microsoft.com>
2016-10-30 14:21:10 -0600

    assigment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+
+##ls()
+##rm(list=ls())
 
 makeCacheMatrix <- function(x = matrix()) {
+	##cache the inverse matrix as a 'global' by using the <<- assignment
+	inv_matrix <<- solve(x)
 
+	inv_matrix
 }
 
+##test 1
+c=rbind(c(1, -1/4), c(-1/4, 1))  
+c
+makeCacheMatrix(c)
+inv_matrix
 
-## Write a short comment describing this function
+## cacheSolve : This function computes the inverse of the special "matrix" 
+## returned by  makeCacheMatrix  above. If the inverse has already been 
+## calculated (and the matrix has not changed), 
+## then  cacheSolve  should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	## If the inverse has already been calculated (and the matrix has not changed)
+       if(!is.null(inv_matrix)) {
+            message("getting cached data")
+  		## then  cacheSolve should retrieve the inverse from the cache.
+		z <- inv_matrix
+	}
+	else 
+	{
+		z <-solve(x)
+	}
+
+	## Return a matrix that is the inverse of 'x'
+	z
 }
+
+
+
+##test2: should return inv_matrix
+##           [,1]      [,2]
+##	[1,] 1.0666667 0.2666667
+##	[2,] 0.2666667 1.0666667
+
+cacheSolve(c)
+
